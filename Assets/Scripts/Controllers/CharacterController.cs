6e7bf2fb090accaf8e3a14014d5de8555c8ee6bf
renz <prencylabueg@gmail.com>
2023-07-08 09:09:39 +0800

    Character and Weapon Controller: Added controllers and animation

---
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float turnRate = 5f;
+    private void Update()
+    {
+        //mouse position on screen
+        Vector3 screenMousePosition = Input.mousePosition;
+
+        //mouse position on world
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(screenMousePosition);
+        worldMousePosition.z = 0f;
+
+        //normalize the direction to get a value between 0 and 1
+        Vector3 direction = worldMousePosition - transform.position;
+        direction.Normalize();
+
+        //angle of rotation - 90f to get the top part of the screen as the initial rotation
+        float angleRotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+
+        Quaternion targetRotation = Quaternion.AngleAxis(angleRotation, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnRate * Time.deltaTime);
+    }
+}
