486d0dff25cdec8d3a50647f996b573f4d944608
renz <prencylabueg@gmail.com>
2023-07-08 10:04:36 +0800

    character and weapon controller: added a isGamePaused condition for all update functions

--- Assets/Scripts/Utilities/CooldownBarUI.cs
+++ Assets/Scripts/Utilities/CooldownBarUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CooldownBarUI : MonoBehaviour
+public class CooldownBarUI : ManagerBehaviour
 {
     [SerializeField] private Transform _targetObject;
 
@@ -15,11 +15,20 @@ public class CooldownBarUI : MonoBehaviour
 
     private void Update()
     {
+        if (!GameManager.IsGamePaused)
+        {
+            UpdateCooldownBarUI();
+        }
+    }
+
+    private void UpdateCooldownBarUI ()
+    {
         if (_targetObject != null)
         {
             // Update the UI position to match the target object
+
             Vector3 targetPosition = _targetObject.position;
-            Vector3 screenPosition = targetPosition + new Vector3(0f,-0.604f,0f);
+            Vector3 screenPosition = targetPosition + new Vector3(0f, -0.604f, 0f);
             _uiRectTransform.position = screenPosition;
         }
     }
