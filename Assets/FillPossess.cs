654d19a4f3c265e25daa4f87429dbbac260221b5
Piolo-Ambion <pafambion@gmail.com>
2023-07-09 22:00:08 +0800

    possessiongauge-and-crosshair: added possession gauge and changes cursor in-game

--- Assets/FillPossess.cs
+++ Assets/FillPossess.cs
@@ -3,32 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FillPossess : MonoBehaviour
+public class FillPossess : ManagerBehaviour
 {
     public Image ImageFill;
     public float GuageAmount = 5;
     
     void Update()
     {
-        ImageFill.fillAmount = GuageAmount / 5;
+        ImageFill.fillAmount = (float)GameManager.PossessionGauge / 5;
 
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            PossessPoint(1);
-            //ano lng to uhh to show yung fill
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GuageAmount = 0;
-            //then probably a code to reset when possessed to go back 0
-
-        }
     }
-
-    public void PossessPoint(float FillGuage)
-    {
-        GuageAmount += FillGuage;
-    }
-
-    
 }
